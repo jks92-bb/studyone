b2fb18d4a34d5b4bfb0c7fe2d64a8b1d14e7aa49
jks92-bb <obhop92@gmail.com>
2023-11-07 16:31:38 +0900

    SSMS (CarManager) 231107

---
+++ SSMS/SQLQuery1.sql
@@ -0,0 +1,11 @@
+use CarManager; -- DB선택해줘야 함.
+
+select * from ParkingCarManager; -- 테이블 조회
+--insert into ParkingCarManager
+--(parkingSpot,CarNumber,DriverName,PhoneNumber) -- 테이블 데이터 칼럼 
+--values('5','12나3245','김장수','010-4432-4553')
+--delete from ParkingCarManager;
+--update ParkingCarManager set DriverName ='유재석'
+--where parkingSpot = '5';
+select *from ParkingCarManager;
+
