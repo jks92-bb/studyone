8da8a33ca54680fc49cd6746fc05b433a8ce65c4
jks92-bb <obhop92@gmail.com>
2023-12-26 20:17:07 +0900

    api key  제거

--- Python/mapletest.py
+++ Python/mapletest.py
@@ -12,7 +12,7 @@ print(yesterday_str)
 
 def get_character_data(character_name):
     headers = {
-        "x-nxopen-api-key": "test_e6f5684cbfb6237d5d3ff8740d5ee712aeb8ddd9ba8877df5e1d291949e7e6a280553df27fa82c9e2a239d36cabd158b"
+        "x-nxopen-api-key": "test"
     }
 
     url_string = f"https://open.api.nexon.com/maplestory/v1/id?character_name={character_name}"
