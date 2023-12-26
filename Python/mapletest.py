2d16d9c33efbfe92ff7b93828272add864f74efc
jks92-bb <obhop92@gmail.com>
2023-12-26 10:20:33 +0900

    maple test

--- Python/mapletest.py
+++ Python/mapletest.py
@@ -1,8 +1,9 @@
 import requests
-import json
+
+
 
 headers = {
-  "x-nxopen-api-key": "test_e6f5684cbfb6237d5d3ff8740d5ee712aeb8ddd9ba8877df5e1d291949e7e6a280553df27fa82c9e2a239d36cabd158b"
+  "x-nxopen-api-key": "test_e6f5684cb158b"
 }
 
 characterName = input("캐릭터명: ")
