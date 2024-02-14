15e7f1c7a66d6055686916ae87b8360053db1ea9
jks92-bb <obhop92@gmail.com>
2024-02-14 15:45:01 +0900

    파이썬 업데이트(넥슨openAPI, 대구날씨)

--- Python/mapletest.py
+++ Python/mapletest.py
@@ -10,9 +10,17 @@ yesterday_str= yesterday.strftime("%Y-%m-%d")
 
 print(yesterday_str)
 
+import requests
+from datetime import datetime, timedelta
+
+def get_yesterday_date():
+    # 어제의 날짜를 "yyyy-mm-dd" 형식으로 계산
+    yesterday_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    return yesterday_date
+
 def get_character_data(character_name):
     headers = {
-        "x-nxopen-api-key": "test"
+        "x-nxopen-api-key": "ㅏ"
     }
 
     url_string = f"https://open.api.nexon.com/maplestory/v1/id?character_name={character_name}"
@@ -25,13 +33,12 @@ def get_character_data(character_name):
     except requests.exceptions.RequestException as err:
         return f"Error: {err}"
 
-
-def get_character_basic(character_basic):
+def get_character_basic(ocid, date):
     headers = {
-        "x-nxopen-api-key": "test_e6f5684cbfb6237d5d3ff8740d5ee712aeb8ddd9ba8877df5e1d291949e7e6a280553df27fa82c9e2a239d36cabd158b"
+        "x-nxopen-api-key": "ㅏ"
     }
 
-    url_string = f"https://open.api.nexon.com/maplestory/v1/id?character_name={character_name}"
+    url_string = f"https://open.api.nexon.com/maplestory/v1/character/basic?ocid={ocid}&date={date}"
     response = requests.get(url_string, headers=headers)
 
     try:
@@ -40,22 +47,18 @@ def get_character_basic(character_basic):
 
     except requests.exceptions.RequestException as err:
         return f"Error: {err}"
-
-
-
-
-
 
 def main():
     characterName = input("캐릭터명: ")
     character_data = get_character_data(characterName)
 
-    # 'ocid' 키의 값을 추출하여 character_basic 변수에 저장
-   # character_basic = get_character_basic('ocid', '')
-
     if isinstance(character_data, dict):
-        print('식별자',character_data)
-       # print('캐릭터 기본 정보:', character_basic)
+        ocid = character_data.get('ocid', '')
+        yesterday_date = get_yesterday_date()
+        character_basic = get_character_basic(ocid, yesterday_date)
+
+        print('식별자:', character_data)
+        print('캐릭터 기본 정보:', character_basic)
         print('Data based on NEXON Open API')
     else:
         print(character_data)
