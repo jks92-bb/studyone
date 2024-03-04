aedf1841169c151259b64a3a098f2193e9b51409
jks92-bb <obhop92@gmail.com>
2024-03-04 11:17:27 +0900

    LOTTO추가

---
+++ C#/Lotto(240304)/Lotto(231106)/Form1.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lotto_231106_
+{
+    public partial class Form1 : Form
+    {
+        private int stnumber ;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Lotto> lottos = new List<Lotto>();
+        
+            while (true)
+            {
+                //using 영역이 끝나면 wc의 메모리가 자동 해제됨.
+                using (WebClient wc = new WebClient())
+                {
+                    var json = wc.DownloadString("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + stnumber);
+                    var jArray = JObject.Parse(json);
+                    if (jArray["returnValue"].ToString() == "fail")
+                        break;
+                    Lotto temp = new Lotto(
+                        jArray["drwtNo1"].ToString(),
+                        jArray["drwtNo2"].ToString(),
+                        jArray["drwtNo3"].ToString(),
+                        jArray["drwtNo4"].ToString(),
+                        jArray["drwtNo5"].ToString(),
+                        jArray["drwtNo6"].ToString(),
+                        jArray["bnusNo"].ToString(),
+                        jArray["drwNo"].ToString());
+                    lottos.Add(temp);
+                    stnumber++;
+                }
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = lottos;
+        }
+                
+
+        private void Number_input(object sender, EventArgs e)
+        {
+            if (int.TryParse(numberbox.Text, out int inputNumber))
+            {
+                stnumber = inputNumber;
+            }
+            else
+            {
+                MessageBox.Show("숫자를 입력하세요.");
+            }
+        }
+
+       
+    }
+}
